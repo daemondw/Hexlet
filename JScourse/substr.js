270584fa22398e4486c72e9229f8c28e851b4c23
Dima Antonenko <daemondw@gmail.com>
2018-09-09 01:28:38 +0200

    subString full

--- JScourse/substr.js
+++ JScourse/substr.js
@@ -1,3 +1,17 @@
-const substr = (str) => {
-  
+const substr = (str, ind = 0, length = str.length) => {
+  if (length < 0) {
+    return length = 1;
+  } else if (length > str.length) {
+    return length = str.length;
+  } else if (length === 0) {
+    return length = '';
+  } else if (ind > str.length || ind < 0 && length > str.length || length < 0) {
+    return substr(str, 0, str.length);
+  } else if (ind < 0) {
+    return ind = 0;
+  } else if (ind > str.length) {
+    return ind = '';
+  }
+    return length = str.length - ind;
 };
+export default substr;
