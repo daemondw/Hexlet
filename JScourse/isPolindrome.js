23e648d88c8b3d495e88adf5c9fa772aa9f96390
Dima Antonenko <daemondw@gmail.com>
2018-09-08 21:53:11 +0200

    polindrome finish

--- JScourse/isPolindrome.js
+++ JScourse/isPolindrome.js
@@ -1,7 +1,8 @@
 const isPalindrome = (str) => {
-if (str.length < 2) {
-  return true;
-} else if (str[0] !== str.substr(-1, 1)) {
-
-}
-};
+  if (str.length < 2) {
+    return true;
+    } else if (str.substr(0, 1) !== str.substr(-1, 1)) {
+      return false;
+      }
+      return isPalindrome(str.substr(1, str.length - 2));
+      };
