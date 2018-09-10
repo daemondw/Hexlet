09b6d518e89fdc345516ad7a300f3354bb46e8db
Dima Antonenko <daemondw@gmail.com>
2018-09-10 14:12:26 +0200

    perfektNum was finished

--- JScourse/isPerfekt.js
+++ JScourse/isPerfekt.js
@@ -1,12 +1,15 @@
 const isPerfect = (num) => {
-    const sum = (begin, end) => {
-      if (begin === end) {
-        return begin;       
-      }
-      else {
-        return begin + sum(begin + 1 , end);
-      }      
-    } 
-return sum(1, num) / num === 1;
-}
-isPerfect(6);
+  let res = 0;
+  if (num < 6) {
+    return false;
+  }
+  for (let i = 1; i < num; i += 1) {
+    if (num % i === 0) {
+      res += i;
+    }
+  }
+  if (res === num) {
+    return true;
+  }
+  return false;
+};
