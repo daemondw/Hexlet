e4f007b7de38e5c3aa739d22425284c402613a2c
Dima Antonenko <daemondw@gmail.com>
2018-09-12 13:42:36 +0200

    invert Register finished

--- JScourse/invertCase.js
+++ JScourse/invertCase.js
@@ -6,11 +6,9 @@ const invertCase = (str) => {
   for (let i = 0; i < str.length; i += 1) {
     if (str.charAt(i) === str[i].toUpperCase()) {
       strNew += str.charAt(i).toLowerCase();
-      console.log(strNew);
-    } else {
-      strNew += str.charAt(i);
-      console.log(strNew);
+    } else if (str.charAt(i) !== str[i].toUpperCase()) {
+      strNew += str.charAt(i).toUpperCase();
     }
   }
-    return strNew;
+  return strNew;
 };
