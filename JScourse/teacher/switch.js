9065a4d35336dc5e1b4deacf4801b15d2b4e6451
Dima Antonenko <daemondw@gmail.com>
2018-09-17 13:30:56 +0200

    switch

---
+++ JScourse/teacher/switch.js
@@ -0,0 +1,14 @@
+const calc = (operation, a, b) => {
+  switch (operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '/':
+      return a / b;
+    case '*':
+      return a * b;
+    default:
+      return NaN;
+  }
+};
