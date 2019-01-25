8958f5014f794f0c5eb31964e9ca558ad2219cb2
Dima Antonenko <daemondw@gmail.com>
2019-01-25 13:57:03 +0100

    git ignore file

--- new_Project/babel.config.js
+++ new_Project/babel.config.js
@@ -1,11 +1,12 @@
-module.exports = function (api) {
-    api.cache(true);
-  
-    const presets = [ ... ];
-    const plugins = [ ... ];
-  
-    return {
-      presets,
-      plugins
-    };
-  }
+module.exports = {
+  presets: [
+    ['@babel/env', {
+      targets: {
+        node: 'current',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1',
+      },
+    }],
+  ],
+};
